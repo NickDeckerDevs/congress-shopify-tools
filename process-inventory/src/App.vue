4d4527221bc3d11dcc8267098dc858e1182c86ef
nicholas decker <nicholas@deckerdevs.com>
2020-01-10 21:44:36 -0500

    initial vue app creation and form component

---
+++ process-inventory/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <h1>Upload your CSV</h1>
+    <file-upload />
+    
+  </div>
+</template>
+
+<script>
+import FileUpload from './components/FileUpload.vue'
+
+export default {
+  name: 'app',
+  components: {
+    FileUpload
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ process-inventory/src/components/FileUpload.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="file">
+    <form enctype="multipart/form-data">
+      <div class="input-group">
+        <label>File Upload</label>
+        <input type="file" />
+      </div>
+      <div class="input-group">
+        <button>Upload File</button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'FileUpload'
+}
+</script>
